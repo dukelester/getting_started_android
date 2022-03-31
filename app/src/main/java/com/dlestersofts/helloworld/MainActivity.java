c5a541d3eed2d7eb8f0fdf58e359ec82185bc038
dukelester <dukelester4@gmail.com>
2022-03-31 12:17:14 +0300

    Added a button, got user input and a welcome message

--- app/src/main/java/com/dlestersofts/helloworld/MainActivity.java
+++ app/src/main/java/com/dlestersofts/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.dlestersofts.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        TextView textView = findV
+
     }
+    public void greetUser(View view){
+        TextView myText = findViewById(R.id.textView2);
+        EditText userName = findViewById(R.id.editTextName);
+        myText.setText("hello " + userName.getText().toString() + " Welcome to android Development! ");
+
+    }
+
 }
